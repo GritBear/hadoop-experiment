85019dd3b85b3357aaa5a959a063a7b81c4bfc9c
Lee Shiung <fenxls@hotmail.com>
2016-02-02 00:35:58 -0800

    complete lookups

--- scripts/q1-dataset/recommendation.py
+++ scripts/q1-dataset/recommendation.py
@@ -6,7 +6,6 @@ Created on Mon Feb 01 22:29:14 2016
 """
 
 import numpy as np
-import math
 
 def extract_txt_arr(filename):
   with open(filename, 'rb') as f:
@@ -18,10 +17,9 @@ def extract_txt_arr(filename):
     return np.array(data)
     
 alexRaw = extract_txt_arr('alex.txt')
-alexTest = np.copy(alexRaw)
-alexTest[0, 0:100] = 0
 
 R = extract_txt_arr('user-shows.txt')
+R[499,0:100] = 0
 
 m = R.shape[0] 
 n = R.shape[1]
@@ -30,13 +28,35 @@ Pinverse_sqrt = np.zeros((m,m))
 for x in range(0, m):
     Pinverse_sqrt[x,x] = 1/np.sqrt(np.sum(R[x,:]))
 
-Lu = np.dot(np.dot(np.dot(np.dot(Pinverse_sqrt, R),np.transpose(R)),Pinverse_sqrt),R)
+Su = np.dot(np.dot(np.dot(np.dot(Pinverse_sqrt, R),np.transpose(R)),Pinverse_sqrt),R)
+Su_sp = Su[499,0:100]
 
+temp = np.argpartition(-Su_sp, 5)
+kUU = temp[:5]
+print "5 highest indices"
+print kUU
 
-"""
+temp = np.partition(-Su_sp, 5)
+kUU_score = -temp[:5] 
+print "5 highest scores"
+print kUU_score
+
+#now using item to item
+
 Qinverse_sqrt = np.zeros((n,n))
 
 for x in range(0, n):
-    Q[x,x] = np.sum(R[:,x])
-"""   
-#Luu = np.linalg.inv(np.sqrt(P))*R*np.transpose(R)*np.linalg.inv(np.sqrt(P))*R
+    Qinverse_sqrt[x,x] = 1/np.sqrt(np.sum(R[:,x]))
+
+Si = np.dot(R,np.dot(np.dot(np.dot(Qinverse_sqrt, R),np.transpose(R)),Qinverse_sqrt))
+Si_sp = Si[499,0:100]
+
+temp = np.argpartition(-Si_sp, 5)
+kII = temp[:5]
+print "5 highest indices"
+print kII
+
+temp = np.partition(-Si_sp, 5)
+kII_score = -temp[:5] 
+print "5 highest scores"
+print kII_score
